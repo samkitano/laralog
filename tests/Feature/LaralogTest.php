d7cd4bf10dc84f9dd131e463a8c97a13c07f1734
Sam Kitano <sam.kitano@gmail.com>
2018-12-27 13:15:22 +0000

    added first test

---
+++ tests/Feature/LaralogTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace samkitano\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class LaralogTest extends TestCase
+{
+    /** @test */
+    public function first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
